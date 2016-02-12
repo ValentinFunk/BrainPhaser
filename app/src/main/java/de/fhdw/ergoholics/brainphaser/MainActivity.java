00cd272a4855a7a6e285e5dacf83e1dd52cfd871
Valentin Funk <funk.valentin@gmail.com>
2016-02-12 20:17:11 +0100

    Initial commit

---
+++ app/src/main/java/de/fhdw/ergoholics/brainphaser/MainActivity.java
@@ -0,0 +1,23 @@
+package de.fhdw.ergoholics.brainphaser;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
+
+    }
+
+}
