d68a4f7768adfa72d369e206c343ef082f2a709f
Daniel Hoogen <daniel.hoogen@web.de>
2016-02-19 09:56:22 +0100

    Added file association for category import, fixed ImportChallengeActivity class

--- app/src/main/java/de/fhdw/ergoholics/brainphaser/activities/ChallengeImport/ImportChallengeActivity.java
+++ app/src/main/java/de/fhdw/ergoholics/brainphaser/activities/ChallengeImport/ImportChallengeActivity.java
@@ -1,10 +1,18 @@
 package de.fhdw.ergoholics.brainphaser.activities.ChallengeImport;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import de.fhdw.ergoholics.brainphaser.R;
 
 /**
  * Created by Daniel Hoogen on 16/02/2016.
  */
 public class ImportChallengeActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import_challenges);
+
+    }
 }
