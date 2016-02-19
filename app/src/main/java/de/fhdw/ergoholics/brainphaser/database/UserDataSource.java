307075493a12418b4300a19f245ed72515eb4ac8
CruOzer <ChrisKost94@gmail.com>
2016-02-19 12:31:52 +0100

    Error Fixing in UserDataSource and Gradle Update

--- app/src/main/java/de/fhdw/ergoholics/brainphaser/database/UserDataSource.java
+++ app/src/main/java/de/fhdw/ergoholics/brainphaser/database/UserDataSource.java
@@ -59,7 +59,7 @@ public class UserDataSource extends DataSource {
      */
     public User getUser(int id){
         User user;
-        Cursor cursor=mDatabase.query(DatabaseStatics.USER_TABLE_NAME,columns,DatabaseStatics.USER_COL_1 + "=" + id,null,null,null,null);
+        Cursor cursor=mDatabase.query(DatabaseStatics.USER_TABLE_NAME, columns, DatabaseStatics.USER_COL_1 + "=" + id, null, null, null, null);
         user = cursorToUser(cursor);
         cursor.close();
         return user;
@@ -72,7 +72,7 @@ public class UserDataSource extends DataSource {
      */
     public User getUser(String name){
         User user;
-        Cursor cursor=mDatabase.query(DatabaseStatics.USER_TABLE_NAME,columns,DatabaseStatics.USER_COL_2 + "=" + name,null,null,null,null);
+        Cursor cursor=mDatabase.query(DatabaseStatics.USER_TABLE_NAME,columns,DatabaseStatics.USER_COL_2 + "= ?" , new String[]{name},null,null,null);
         user = cursorToUser(cursor);
         cursor.close();
         return user;
@@ -87,7 +87,7 @@ public class UserDataSource extends DataSource {
     public List<User> getUser(String param,String column){
         List<User> allUsers = new ArrayList<User>();
         //cursor on the user table
-        Cursor cursor=mDatabase.query(DatabaseStatics.USER_TABLE_NAME,columns,column + "=" + param,null,null,null,null);
+        Cursor cursor=mDatabase.query(DatabaseStatics.USER_TABLE_NAME,columns,column + "= ?",new String[]{param},null,null,null);
         //reset cursor
         cursor.moveToFirst();
         User user;
