6c463a6fe49c1f30c0bb7381a442570eac3d2cfa
Valentin Funk <funk.valentin@gmail.com>
2016-02-20 13:52:39 +0100

    Add noDisplay activity for selector, add activity to manifest

--- app/src/main/java/de/fhdw/ergoholics/brainphaser/activities/MainActivity.java
+++ app/src/main/java/de/fhdw/ergoholics/brainphaser/activities/MainActivity.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
 
 import de.fhdw.ergoholics.brainphaser.BrainPhaserApplication;
+import de.fhdw.ergoholics.brainphaser.R;
 import de.fhdw.ergoholics.brainphaser.activities.CategorySelect.SelectCategoryActivity;
 import de.fhdw.ergoholics.brainphaser.activities.UserCreation.CreateUserActivity;
 
@@ -17,8 +20,10 @@ import de.fhdw.ergoholics.brainphaser.activities.UserCreation.CreateUserActivity
  */
 public class MainActivity extends Activity {
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.mainactivity);
 
         BrainPhaserApplication application = (BrainPhaserApplication)getApplication();
         if (application.logInLastUser()) {
