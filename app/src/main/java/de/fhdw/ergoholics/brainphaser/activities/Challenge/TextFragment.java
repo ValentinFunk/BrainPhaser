e229f2f7955b91286b8ad53e647202dffaffbb4a
CruOzer <ChrisKost94@gmail.com>
2016-03-03 16:09:51 +0100

    RecyclerView und Adapter zum Ausgeben von Antworten

--- app/src/main/java/de/fhdw/ergoholics/brainphaser/activities/Challenge/TextFragment.java
+++ app/src/main/java/de/fhdw/ergoholics/brainphaser/activities/Challenge/TextFragment.java
@@ -2,11 +2,12 @@ package de.fhdw.ergoholics.brainphaser.activities.Challenge;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-
 import de.fhdw.ergoholics.brainphaser.R;
 import de.fhdw.ergoholics.brainphaser.model.Answer;
 
@@ -15,26 +16,46 @@ import de.fhdw.ergoholics.brainphaser.model.Answer;
  */
 public class TextFragment extends AnswerFragment {
 
-
-    EditText mAnserText;
+    View mView;
+    //Textfield of the answer
+    EditText mAnswerText;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_challenge_multiple_choice, container, false);
-        mAnserText = (EditText) view.findViewById(R.id.answerText);
+        mView = inflater.inflate(R.layout.fragment_challenge_multiple_choice, container, false);
+        mAnswerText = (EditText) mView.findViewById(R.id.answerText);
         loadChallengeAndAnswers();
-        return view;
+        return mView;
 
     }
 
+    /**
+     * Checks the given answer
+     * @return Is the answer right or nah
+     */
     @Override
     public boolean checkAnswers() {
         boolean answerRight=false;
         for (Answer item:mAnswerList) {
-            if(item.getText().equals( mAnserText.getText().toString())){
+            if(item.getText().equals(mAnswerText.getText().toString())){
                 answerRight=true;
             }
         }
+
+        //loading of the components
+        RecyclerView answerList = (RecyclerView) mView.findViewById(R.id.answerList);
+
+        answerList.setHasFixedSize(true);
+
+        // use a linear layout manager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        answerList.setLayoutManager(layoutManager);
+
+        //Create the View
+        //Adapter which sets all answers into the list
+        AnswerAdapter listAdapter = new AnswerAdapter(mAnswerList);
+        answerList.setAdapter(listAdapter);
+
         return answerRight;
     }
 }
