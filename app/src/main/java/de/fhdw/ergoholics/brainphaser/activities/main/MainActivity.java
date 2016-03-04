55171cca29d024bbf110db26823565f90ffe94e6
Valentin Funk <funk.valentin@gmail.com>
2016-03-04 20:19:38 +0100

    Added an easy way to import BPC in Dev Mode

--- app/src/main/java/de/fhdw/ergoholics/brainphaser/activities/main/MainActivity.java
+++ app/src/main/java/de/fhdw/ergoholics/brainphaser/activities/main/MainActivity.java
@@ -2,19 +2,24 @@ package de.fhdw.ergoholics.brainphaser.activities.main;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.nononsenseapps.filepicker.FilePickerActivity;
+
 import de.fhdw.ergoholics.brainphaser.BrainPhaserApplication;
 import de.fhdw.ergoholics.brainphaser.BuildConfig;
 import de.fhdw.ergoholics.brainphaser.R;
+import de.fhdw.ergoholics.brainphaser.activities.ChallengeImport.ImportChallengeActivity;
 import de.fhdw.ergoholics.brainphaser.activities.UserCreation.CreateUserActivity;
 import de.fhdw.ergoholics.brainphaser.activities.UserSelection.UserSelectionActivity;
 
@@ -28,11 +33,19 @@ public class MainActivity extends AppCompatActivity {
     public static String EXTRA_NAVIGATE_TO = "NAVIGATE_TO";
     public static String EXTRA_SHOW_LOGGEDIN_SNACKBAR = "SHOW_SNACKBAR";
 
+    private final static int CODE_FILEPICKER = 0;
+
     private ViewPager mViewPager;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu, menu);
+
+        if (BuildConfig.DEBUG) {
+            MenuItem item = menu.add("Import BPC");
+            item.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
+        }
+
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -46,9 +59,24 @@ public class MainActivity extends AppCompatActivity {
                 // TODO
             case R.id.action_settings:
                 // TODO
+            case 0: // Only in debug mode: File Picker
+                Intent i = new Intent(getApplicationContext(), FilePickerActivity.class);
+                startActivityForResult(i, CODE_FILEPICKER);
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Pass the intent containing the file URI on to the Import Challenge method
+        if (requestCode == CODE_FILEPICKER) {
+            data.setClass(getApplicationContext(), ImportChallengeActivity.class);
+            startActivity(data);
+            return;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
