aec49315b165116cdba7bf8524f7608dc1a37fcf
CruOzer <ChrisKost94@gmail.com>
2016-03-11 11:56:25 +0100

    Self-Check implementiert als Fragment und Listener auf Überprüfung der Antwort. Fehler: FloatingButton immer noch nicht sichtbar aufm S3

--- app/src/main/java/de/fhdw/ergoholics/brainphaser/activities/Challenge/FinishChallengeFragment.java
+++ app/src/main/java/de/fhdw/ergoholics/brainphaser/activities/Challenge/FinishChallengeFragment.java
@@ -1,6 +1,7 @@
 package de.fhdw.ergoholics.brainphaser.activities.Challenge;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,8 @@ import de.fhdw.ergoholics.brainphaser.R;
  */
 public class FinishChallengeFragment extends Fragment {
 
+    @Nullable
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_finish_challenge, container,false);
         return view;
---
+++ app/src/main/java/de/fhdw/ergoholics/brainphaser/activities/Challenge/SelfTestDialogFragment.java
@@ -0,0 +1,50 @@
+package de.fhdw.ergoholics.brainphaser.activities.Challenge;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import de.fhdw.ergoholics.brainphaser.BrainPhaserComponent;
+import de.fhdw.ergoholics.brainphaser.R;
+
+/**
+ * Created by Chris on 3/10/2016.
+ */
+public class SelfTestDialogFragment extends AnswerFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_challenge_self_test, container, false);
+        Button btnRight = (Button)mView.findViewById(R.id.answerRight);
+        Button btnWrong = (Button)mView.findViewById(R.id.answerWrong);
+
+        btnRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListener.onAnswerChecked(true);
+            }
+        });
+        btnWrong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mListener.onAnswerChecked(false);
+            }
+        });
+
+        loadAnswers(R.id.answerListSelfCheck);
+        return mView;
+    }
+
+    @Override
+    public void checkAnswers() {
+        return;
+    }
+
+    @Override
+    protected void injectComponent(BrainPhaserComponent component) {
+        component.inject(this);
+    }
+}
