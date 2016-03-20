5e192a36d337db6dfc28a6073b8a388ce63fc0be
CruOzer <ChrisKost94@gmail.com>
2016-03-20 18:56:50 +0100

    Comments

--- app/src/main/java/de/fhdw/ergoholics/brainphaser/activities/BrainPhaserActivity.java
+++ app/src/main/java/de/fhdw/ergoholics/brainphaser/activities/BrainPhaserActivity.java
@@ -8,12 +8,17 @@ import de.fhdw.ergoholics.brainphaser.BrainPhaserComponent;
 
 /**
  * Created by funkv on 06.03.2016.
- *
+ * <p/>
  * Base Activity class to be used by all activitites in the project.
  * Subclasses need to implement injectComponent to use the Depency Injector.
  * See: https://blog.gouline.net/2015/05/04/dagger-2-even-sharper-less-square/
  */
 public abstract class BrainPhaserActivity extends AppCompatActivity {
+    /**
+     * OnCreate injects components
+     *
+     * @param savedInstanceState Ignored
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +28,7 @@ public abstract class BrainPhaserActivity extends AppCompatActivity {
     /**
      * Called to inject dependencies. Should call component.inject(this) as
      * uniform implementation in all Activities.
+     *
      * @param component
      */
     protected abstract void injectComponent(BrainPhaserComponent component);
